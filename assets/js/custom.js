e511c565152e265b9062ef27baeaa560b48ef0c0
salus-sage <salus.sage@gmail.com>
2022-05-12 16:58:39 +0530

    Add mukunda bio + layout changes to cards with collapse

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
-function myFunction() {
- 	var dots = document.getElementById("dots");
-  	var moreText = document.getElementById("more");
-  	var btnText = document.getElementById("myBtn");
+$(document).ready(function () {
+    $('.nav-toggle').click(function (e) {
+        e.preventDefault();
+        var collapse_content_selector = $(this).attr('data-toggle');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('Read More');
+            } else {
+                toggle_switch.html('Read Less');
+            }
+        });
+    });
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
+});
